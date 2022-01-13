30dfb50f9681565aa48fd4152f48bdfd185c8ce3
lihaybs <lihaybs94@gmail.com>
2022-01-14 00:47:57 +0200

    add img + names of track and artist

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,10 +1,53 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import useAuth from "./useAuth"
+import TrackSearchResult from "./TrackSearchResult"
 import { Container, Form } from 'react-bootstrap'
+import SpotifyWebApi from "spotify-web-api-node"
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'a9558e0a39c242d49c5f3557300ef7e2'
+})
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
     const [search, setSearch] = useState("")
+    const [searchResults, setSearchResults] = useState([])
+
+    useEffect(() => {
+        if (!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+    }, [accessToken])
+
+    useEffect(() => {
+        if (!search) return setSearchResults([])
+        if (!accessToken) return
+
+        let cancel = false
+        spotifyApi.searchTracks(search).then(res => {
+            if (cancel) return
+            setSearchResults(
+                res.body.tracks.items.map(track => {
+                    const smallestAlbumImage = track.album.images.reduce(
+                        (smallest, image) => {
+                            if (image.height < smallest.height) return image
+                            return smallest
+                        },
+                        track.album.images[0]
+                    )
+
+                    return {
+                        artist: track.artists[0].name,
+                        title: track.name,
+                        uri: track.uri,
+                        albumUrl: smallestAlbumImage.url,
+                    }
+                })
+            )
+        })
+
+        return () => (cancel = true)
+    }, [search, accessToken])
+
     return (
         <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
             <Form.Control
@@ -13,6 +56,12 @@ export default function Dashboard({ code }) {
                 value={search}
                 onChange={e => setSearch(e.target.value)}
             />
+            <div className="flex-grow-1 my-2" style={{ overflowY: 'auto' }}>
+                {searchResults.map(track => (
+                    <TrackSearchResult track={track} key={track.uri} />
+                ))}
+            </div>
+            <div>Bottom</div>
         </Container>
     )
 }
